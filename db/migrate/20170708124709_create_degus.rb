1651c033d97eb9ca0dc7df11abc3304eb0c18efc
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-08 14:06:12 +0000

    degusのis_makeをis_maleに修正

--- db/migrate/20170708124709_create_degus.rb
+++ db/migrate/20170708124709_create_degus.rb
@@ -2,7 +2,7 @@ class CreateDegus < ActiveRecord::Migration[5.1]
   def change
     create_table :degus do |t|
       t.string  :name,     :null => false
-      t.boolean :is_make,  :null => false
+      t.boolean :is_male,  :null => false
       t.date    :birthday, :null => false
       t.date    :comeDate, :null => true
       t.date    :diedDate, :null => false
