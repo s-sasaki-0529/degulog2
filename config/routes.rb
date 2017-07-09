6bb4572dc48ad49291ec217d4ae1156345d02caa
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-09 09:45:01 +0000

    DEV-194 とりあえずVueとBlumaをCDNからインクルード

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root :to => 'home#index'
+  get 'home/index'
+
   namespace :api, defaults: { format: :json } do
     resources :degus, only: [:index, :show, :create, :update, :destroy]
     resources :weights, only: [:index, :create, :update, :destroy]
