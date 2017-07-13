5e30025ba6cdb1b4d6be3f3bacfc81368b077e39
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-13 15:39:18 +0000

    webpackを導入し、JavaScriptを一元化

---
+++ app/javascript/degus.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+var vm = new Vue({
+  el: '#degus',
+  data: {
+    degus: [
+      {name: 'パズー', sex: 'オス', age: '1歳10ヶ月'},
+      {name: 'パズー', sex: 'オス', age: '1歳10ヶ月'},
+      {name: 'パズー', sex: 'オス', age: '1歳10ヶ月'},
+      {name: 'パズー', sex: 'オス', age: '1歳10ヶ月'},
+    ],
+  },
+  methods: {
+  }
+});
