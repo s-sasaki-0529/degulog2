3a7456b9049996f651349d5c6c18e928849c41f7
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-15 09:43:06 +0000

    vueRouterを使った最小限のSPAを実装

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,3 +1,25 @@
-import vue        from 'vue';
-import vueRouter  from 'vue-router';
+import Vue        from 'vue';
+import VueRouter  from 'vue-router';
 import http       from './http';
+Vue.use(VueRouter);
+
+// ページごとのコンポーネントを定義
+const Top     = { template: '<div>Top</div>' };
+const Degus   = { template: '<div>Degus</div>' };
+const Weights = { template: '<div>Weights</div>' };
+const Boughts = { template: '<div>Boughts</div>' };
+const Tweets  = { template: '<div>Tweets</div>' };
+
+// URLに対応するコンポーネントを定義
+const routes = [
+  { path: '/',        component: Top   },
+  { path: '/degus',   component: Degus },
+  { path: '/weights', component: Weights },
+  { path: '/boughts', component: Boughts },
+  { path: '/tweets',  component: Tweets },
+];
+
+// ページ全体をVueコンポーネント化
+const app = new Vue({
+  router: new VueRouter({routes}),
+}).$mount('#app');
