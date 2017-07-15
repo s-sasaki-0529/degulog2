c8f052e10e1eb500e50a0bba85315828f395a9d4
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-15 09:08:45 +0000

    DEV-208 webpackconfigにコメント追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 module.exports = {
+  // webpackが読み込むスタート地点
   entry: __dirname + '/app/javascript/main.js',
+  // ビルド後の出力先
   output: {
     path: __dirname + '/app/assets/javascripts',
     filename: 'application.js',
   },
+  // vuejsを事前にコンパイル(?)
   resolve: {
     alias: {
       vue: 'vue/dist/vue.js',
