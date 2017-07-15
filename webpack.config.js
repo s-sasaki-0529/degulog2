a42a9f9fe3a0428535eabf9a08450b4b760d16c3
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-15 17:23:29 +0000

    DEV-213 webpackでsaasもビルドできるように

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
+const path = require('path');
+
 module.exports = {
   // webpackが読み込むスタート地点
-  entry: __dirname + '/app/javascript/main.js',
+  entry: [
+    __dirname + '/app/javascript/main.js',
+    __dirname + '/app/stylesheet/application.scss',
+  ],
   // ビルド後の出力先
   output: {
     path: __dirname + '/app/assets/javascripts',
     filename: 'application.js',
   },
   module: {
-    // .vueファイルを変換
     loaders: [
+      // .vueファイルを変換
       { test: /\.vue$/, loader: 'vue-loader' },
+      // scssファイルをcssに変換
+      { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
     ],
   },
   // vuejsを事前にコンパイル(?)
   resolve: {
     alias: {
       vue: 'vue/dist/vue.js',
-    }
-  }
-}
+    },
+  },
+};
