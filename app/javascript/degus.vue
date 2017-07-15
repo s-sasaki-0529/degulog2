267458350dfd627908311db3d52cd53da9e38f0e
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-15 14:02:05 +0000

    デグー一覧で、死亡済みのデグーは色を付けるように修正

--- app/javascript/degus.vue
+++ app/javascript/degus.vue
@@ -1,25 +1,31 @@
+<style>
+.degus tr.is-died {
+  background-color: #eee;
+}
+</style>
+
 <template>
 
-<table class='table table-striped'>
-  <thead>
-    <tr>
-      <th>名前</th>
-      <th>性別</th>
-      <th>年齢</th>
-      <th>体重</th>
-      <th>お迎え日</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for='degu in degus'>
-      <td>{{ degu.name }}</td>
-      <td>{{ degu.sex  }}</td>
-      <td>{{ degu.age  }}</td>
-      <td>{{ degu.weight }}g</td>
-      <td>{{ degu.come_date }}</td>
-    </tr>
-  </tbody>
-</table>
+<div class='degus'>
+  <table class='table'>
+    <thead>
+      <tr>
+        <th>名前</th>
+        <th>性別</th>
+        <th>年齢</th>
+        <th>体重</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for='degu in degus' v-bind:class='{"is-died": !degu.is_alive}'>
+        <td>{{ degu.name }}</td>
+        <td>{{ degu.sex  }}</td>
+        <td>{{ degu.age  }}</td>
+        <td>{{ degu.weight }}g</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 
 </template>
 
