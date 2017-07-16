7c0a9d8e2f699e0efdc207c0fd34bcfbdc5a3a3b
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-16 14:01:54 +0000

    体重測定一覧を取得するAPIを、日付降順なるように修正

--- app/controllers/api/measurements_controller.rb
+++ app/controllers/api/measurements_controller.rb
@@ -3,7 +3,7 @@ class Api::MeasurementsController < Api::ApplicationController
   # 体重測定の一覧を取得
   #------------------------------------
   def index
-    render json: Measurement.all
+    render json: Measurement.order(date: :desc)
   end
 
 end
