a86400f41ca7680cb4712979ef5d46504390218d
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-16 14:01:32 +0000

    コミット漏れ

---
+++ app/javascript/measurements.vue
@@ -0,0 +1,35 @@
+<template>
+
+<div class="measurements">
+  
+</div>
+
+</template>
+
+<script>
+  import http from './common/http';
+  export default {
+    data: function () {
+      return {
+        degus:        [],
+        measurements: [],
+      };
+    },
+    methods: {
+      getDegus() {
+        http.getDegs((err, data) => {
+          this.measurements = data.body;
+        });
+      },
+      getMeasurements() {
+        http.getMeasurements((err, data) => {
+          this.measurements = data.body;
+        });
+      },
+    },
+    created: function() {
+      this.getDegus();
+      this.getMeasurements();
+    },
+  }
+</script>
