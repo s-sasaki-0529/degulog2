f81b33fdb6ed9b28628ba78418170eae0e0dc8c3
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-22 16:59:45 +0000

    Tweetsの同記事に、全レコードを削除するのでなく、origin_idで比較して更新するように修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,8 +5,9 @@ class Tweet < ApplicationRecord
   def self.synchronize
     tl = Twitter.new.timeline
     self.transaction do
-      self.delete_all
-      self.create(tl)
+      tl.each do |t|
+        self.find_or_initialize_by(origin_id: t[:origin_id]).update(t)
+      end
     end
   end
 
