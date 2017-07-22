128a2a4798a25812057a6fef2c1cdbaca5f40c3e
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-22 13:59:58 +0000

    デグー関連購入履歴に累計額を表示し、スタイリング微修正

--- app/javascript/boughts.vue
+++ app/javascript/boughts.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="container bought">
+  <div class="container boughts">
     <h1>デグー関連購入履歴</h1>
-    <table class="table table-stripped">
+    <div class="total">
+      <p>累計: {{ totalAmount }}円</p>
+    </div>
+    <table class="table table-bordered">
       <thead>
         <tr>
           <th>日付</th>
@@ -10,12 +13,14 @@
           <th>購入内容</th>
         </tr>
       </thead>
-      <tr v-for="bought in boughts">
-        <td>{{ bought.date }}</td>
-        <td>{{ bought.amount }}</td>
-        <td>{{ bought.place }}</td>
-        <td>{{ bought.comment }}</td>
-      </tr>
+      <tbody>
+        <tr v-for="bought in boughts">
+          <td>{{ bought.date }}</td>
+          <td class="amount">{{ bought.amount }}円</td>
+          <td>{{ bought.place }}</td>
+          <td>{{ bought.comment }}</td>
+       </tr>
+      </tbody>
     </table>
   </div>
 </template>
@@ -34,6 +39,13 @@
           this.boughts = data.body;
         });
       },
+    },
+    computed: {
+      totalAmount: function() {
+        let total_amount = 0;
+        this.boughts.forEach((b) => total_amount += b.amount);
+        return total_amount;
+      }
     },
     created: function() {
       this.getBoughts();
