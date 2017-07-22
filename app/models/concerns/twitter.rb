0cf5de0ca46a5c23ce29770a0b19c62ad457c906
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-22 16:55:03 +0000

    Twitterから#デグーのツイートを抜き出してDBに保存するメソッドを実装

--- app/models/concerns/twitter.rb
+++ app/models/concerns/twitter.rb
@@ -1,6 +1,8 @@
 require 'twitter_oauth'
 class Twitter
 
+  @@SCREEN_NAME = 'housoup'
+  @@KEY_WORD    = '#デグー'
   @@SLEEP_TIME = 2
 
   # initialize - インスタンスを生成する
@@ -9,9 +11,31 @@ class Twitter
     @twitter = TwitterOAuth::Client.new(
       :consumer_key    => ENV['TWITTER_API_KEY'],
       :consumer_secret => ENV['TWITTER_API_SECRET'],
-      :token           => ENV['TWITTER_ACCESS_TOKEN'],
-      :secret          => ENV['TWITTER_ACCESS_SECRET'],
     )
   end
+
+  def timeline
+    params = {
+      screen_name:     @@SCREEN_NAME,
+      count:           200,
+      trim_user:       true,
+      exclude_replies: true,
+      include_rts:     false,
+    }
+    tl = @twitter.user_timeline(params).select {|t| t['text'].index(@@KEY_WORD)}
+    tl.map {|t| parse(t)}
+  end
+
+  private
+
+    def parse(tweet)
+      {
+        origin_id:     tweet['id'],
+        text:          tweet['text'].gsub("\n", '').gsub(/https:.+? *$/, ''),
+        like_count:    tweet['favorite_count'],
+        retweet_count: tweet['retweet_count'],
+        datetime:      tweet['created_at'],
+      }
+    end
 
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,13 @@
 class Tweet < ApplicationRecord
-  has_many: picture
+  has_many :picture
+
+  # TwitterAPIを用いて同期
+  def self.synchronize
+    tl = Twitter.new.timeline
+    self.transaction do
+      self.delete_all
+      self.create(tl)
+    end
+  end
+
 end
