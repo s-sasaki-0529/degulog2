8d9fd6668300427f8fe7a8fccf3788ac61971eb3
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-29 15:17:09 +0000

    ツイートを任意の列で並び替えられるように

--- app/javascript/tweets.vue
+++ app/javascript/tweets.vue
@@ -4,10 +4,10 @@
     <table class="table table-hover pointer">
       <thead>
         <tr>
-          <th class="date">日付</th>
-          <th class="tweet">ツイート</th>
-          <th class="like nowrap">いいね</th>
-          <th class="retweet nowrap">リツイート</th>
+          <th class="date" v-on:click="sort('date')">日付</th>
+          <th class="tweet" v-on:click="sort('text')">ツイート</th>
+          <th class="like nowrap" v-on:click="sort('like_count')">いいね</th>
+          <th class="retweet nowrap" v-on:click="sort('retweet_count')">リツイート</th>
         </tr>
       </thead>
       <tbody>
@@ -32,15 +32,21 @@
       }
     },
     methods: {
+      // APIからツイート一覧を取得
       getTweets() {
         http.getTweets((err, data) => {
           this.tweets = data.body;
         })
       },
+      // 対象の元ツイートを開く
       move(tweet) {
         const url = 'https://twitter.com/HousouP/status/' + tweet.origin_id;
         window.open(url);
       },
+      // ツイートの並び順を変更
+      sort(key) {
+        this.tweets = this.tweets.sort((a, b) => a[key] <= b[key] ? 1 : -1);
+      }
     },
     created: function() {
       this.getTweets();
