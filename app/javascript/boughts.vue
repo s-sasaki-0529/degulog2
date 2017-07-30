2346c02db09ca638d3fcd12a6f586a49d21263e8
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-07-30 13:18:01 +0000

    購入履歴画面にタブを導入

--- app/javascript/boughts.vue
+++ app/javascript/boughts.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container boughts">
+    <!-- ヘッダー -->
     <h1>デグー関連購入履歴</h1>
     <div class="right">
       <button class="btn btn-primary" v-bind:disabled="synchronizing" v-on:click="getLatestBoughts">
@@ -7,24 +8,44 @@
       </button>
       <p>累計: {{ totalAmount }}円</p>
     </div>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>日付</th>
-          <th>支払額</th>
-          <th>購入店舗</th>
-          <th>購入内容</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="bought in boughts">
-          <td>{{ bought.date }}</td>
-          <td class="amount">{{ bought.amount }}円</td>
-          <td>{{ bought.place }}</td>
-          <td>{{ bought.comment }}</td>
-       </tr>
-      </tbody>
-    </table>
+
+    <!-- タブメニュー -->
+    <ul class="nav nav-tabs nav-justified">
+      <li v-bind:class="{'active': selected_tab === 'history'}" v-on:click="changeTab('history')">
+        <a>一覧</a>
+      </li>
+      <li v-bind:class="{'active': selected_tab === 'monthly'}" v-on:click="changeTab('monthly')">
+        <a>月ごと</a>
+      </li>
+    </ul>
+
+    <!-- 購入履歴一覧 -->
+    <div class="history" v-show="selected_tab === 'history'">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>日付</th>
+            <th>支払額</th>
+            <th>購入店舗</th>
+            <th>購入内容</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="bought in boughts">
+            <td>{{ bought.date }}</td>
+            <td class="amount">{{ bought.amount }}円</td>
+            <td>{{ bought.place }}</td>
+            <td>{{ bought.comment }}</td>
+         </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <!-- 月ごとの支出一覧 -->
+    <div class="monthly" v-show="selected_tab === 'monthly'">
+      月ごと
+    </div>
+
   </div>
 </template>
 
@@ -34,10 +55,15 @@
     data: function () {
       return {
         boughts: [],
+        selected_tab: 'history',
         synchronizing: false,
       }
     },
     methods: {
+      // タブを切り替え
+      changeTab(new_tab) {
+        this.selected_tab = new_tab;
+      },
       // 支出一覧を取得(キャシュ)
       getBoughts() {
         http.getBoughts((err, data) => {
