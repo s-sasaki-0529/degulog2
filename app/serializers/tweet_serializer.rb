b2e3047a4d9981b76c331fe23a807ef5c73878c8
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-08-10 16:43:40 +0000

    tweet一覧取得APIで関連する画像のURL一覧も戻す

--- app/serializers/tweet_serializer.rb
+++ app/serializers/tweet_serializer.rb
@@ -1,5 +1,10 @@
 class TweetSerializer < ActiveModel::Serializer
   attributes :origin_id, :text, :like_count, :retweet_count, :datetime, :date
+  has_many :pictures
+
+  def pictures
+    object.pictures.pluck(:url)
+  end
 
   def date
     object.datetime.to_date.to_s
