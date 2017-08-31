a466071abfe2b9bea94697f4b66aacbccb0d7c7c
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-08-31 14:01:03 +0000

    APIルーティングにweightsのCRUDを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # API
   namespace :api, defaults: { format: :json } do
     resources :degus, only: [:index, :show, :create, :update, :destroy]
-    resources :measurements, only: [:index, :create, :update, :destroy]
+    resources :measurements, only: [:index, :create, :update, :destroy] do
+      resources :weights, only: [:index, :create, :update, :destroy]
+    end
     resources :boughts, only: [:index]
     resources :tweets, only: [:index]
   end
