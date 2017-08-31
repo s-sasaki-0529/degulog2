fa1adf168f25cb9f9a2cff3e047953344be699e0
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-08-31 14:05:52 +0000

    やっぱりweightsを直接APIで読み書きするのは無しに

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,7 @@ Rails.application.routes.draw do
   # API
   namespace :api, defaults: { format: :json } do
     resources :degus, only: [:index, :show, :create, :update, :destroy]
-    resources :measurements, only: [:index, :create, :update, :destroy] do
-      resources :weights, only: [:index, :create, :update, :destroy]
-    end
+    resources :measurements, only: [:index, :create, :update, :destroy]
     resources :boughts, only: [:index]
     resources :tweets, only: [:index]
   end
