53b5c6ab3d22b1f7e699198f66d46e6afd3f447e
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-08-31 15:08:24 +0000

    体重記録を新規登録するAPIを実装

--- app/controllers/api/measurements_controller.rb
+++ app/controllers/api/measurements_controller.rb
@@ -2,9 +2,31 @@ class Api::MeasurementsController < Api::ApplicationController
 
   #
   # 体重測定の一覧を取得
+  # 関連するweightsもまとめて返却する
   #
   def index
     render json: Measurement.order(date: :desc)
   end
+
+  #
+  # 体重測定記録を作成
+  # 関連するweightsもまとめて作成する
+  #
+  def create
+    @measurement = Measurement.create(measurement_params).weights.create(weights_params[:weights])
+    if @measurement
+      head :created
+    end
+  end
+
+  private
+
+    def measurement_params
+      params.permit(:date)
+    end
+
+    def weights_params
+      params.permit(weights: [:degu_id, :value])
+    end
 
 end
