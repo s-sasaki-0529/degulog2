d35d5d9d5c6770b6c03bafe4920d7c33e2606ae4
shingo sasaki <shingo.sasaki.0529@gmail.com>
2017-10-15 15:27:16 +0000

    2017/10以降のzaim支出は２倍しないように

--- app/models/bought.rb
+++ app/models/bought.rb
@@ -5,9 +5,10 @@ class Bought < ApplicationRecord
   #
   def self.synchronize
     api_response = Zaim.new.getPetsPayments
-    boughts = api_response.map do |r| 
+    bank_merged_date = Date.new(2017, 10, 1)
+    boughts = api_response.map do |r|
       {
-        amount:  r['amount'] * 2, # 折半分
+        amount:  Date.parse(r['date']) < bank_merged_date ? r['amount'] * 2 : r['amount'], # 折半分
         comment: r['comment'],
         place:   r['place'],
         date:    r['date'],
