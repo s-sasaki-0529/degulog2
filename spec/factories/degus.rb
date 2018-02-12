7901bbdf395328b7e4708af6d5c2bdf390aaf1cf
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-02-12 14:54:28 +0000

    DeguFactoryをよりランダムな内容に修正

--- spec/factories/degus.rb
+++ spec/factories/degus.rb
@@ -1,14 +1,10 @@
 FactoryBot.define do
 
   factory :degu do
-    sequence(:name) { |n| "TEST_NAME_#{n}" }
-    gender 'male'
-    birthday '2017-01-01'
-    come_date '2017-01-01'
-
-    trait :female do
-      gender 'female'
-    end
+    name { Faker::Pokemon.name }
+    gender { [:male, :female].sample }
+    birthday { Faker::Date.between(2.years.ago, Date.today)}
+    come_date Date.today
   end
 
 end
