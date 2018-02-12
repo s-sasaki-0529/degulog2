82440fbd90157d4ff21506c00c71ee1684a53d2e
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-02-12 15:17:32 +0000

    degusFactoryを強化

--- spec/factories/degus.rb
+++ spec/factories/degus.rb
@@ -4,7 +4,14 @@ FactoryBot.define do
     name { Faker::Pokemon.name }
     gender { [:male, :female].sample }
     birthday { Faker::Date.between(2.years.ago, Date.today)}
-    come_date Date.today
+
+    after(:build) do |degu|
+      degu.come_date = Faker::Date.between(degu.birthday, Date.today)
+    end
+
+    trait(:invalid) do
+      leave_date Date.today
+    end
   end
 
 end
