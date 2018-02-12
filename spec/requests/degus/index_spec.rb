ed626cadbd3290ab7d3dfda417d385cf4ef6a7c2
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-02-12 16:58:30 +0000

    デグー一覧取得APIのテストを強化

--- spec/requests/degus/index_spec.rb
+++ spec/requests/degus/index_spec.rb
@@ -1,12 +1,48 @@
 RSpec.describe 'Degus', type: :request do
 
-  FactoryBot.create_list(:degu, 10)
+  before do
+    FactoryBot.create_list(:degu, 5)
+    FactoryBot.create_list(:degu, 3, :invalid)
+  end
 
   describe "GET /api/degus" do
-    it '全件取得できる' do
-      get api_degus_path
-      expect(JSON.parse(response.body).count).to eq Degu.count
+
+    before do
+      get api_degus_path, params: params
+      @degus = JSON.parse(response.body)
     end
+
+    context 'パラメータを指定しない場合' do
+      let(:params) { {} }
+      it '全件取得できる' do
+        expect(@degus.count).to eq Degu.count
+      end
+    end
+
+    context 'valid = 1を指定すると' do
+      let(:params) { {valid: 1} }
+      it '有効なデグーのみ取得できる' do
+        expect(@degus.count).to eq Degu.where(leave_date: nil).count
+      end
+    end
+
+    context 'invalid = 1を指定すると' do
+      let(:params) { {invalid: 1} }
+      it '無効なデグーのみ取得できる' do
+        expect(@degus.count).to eq Degu.where.not(leave_date: nil).count
+      end
+    end
+
+    context 'order = come_dateを指定すると' do
+      let(:params) { {order: 'come_date'} }
+      it '誕生日降順で取得できる' do
+        @degus.inject(@degus.first) do |prev, current|
+          expect(current['come_date']).to be >= prev['come_date']
+          current
+        end
+      end
+    end
+
   end
 
 end
