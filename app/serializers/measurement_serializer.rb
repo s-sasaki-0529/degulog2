a6669c2735a295860ff3ab99a72049578573b192
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-02-14 14:09:07 +0000

    コメント追加

--- app/serializers/measurement_serializer.rb
+++ app/serializers/measurement_serializer.rb
@@ -1,10 +1,17 @@
 class MeasurementSerializer < ActiveModel::Serializer
   attributes :date, :weights
 
+  #
+  # 計測簿をフォーマットした文字列
+  #
   def date
     object.date.strftime('%Y/%m/%d')
   end
 
+  #
+  # デグーIDごとの体重記録
+  # weightsモデルに依存するので注意
+  #
   def weights
     weights_hash  = {}
     object.weights.each do |o|
