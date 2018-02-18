8d9fbaf8782802ae69c2628f7a22f1e906702585
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-02-18 14:44:14 +0000

    clean boughtクラスをリファクタリング
    synchronizeメソッドから、支払日に応じた支出額を戻す処理をプライベートクラスメソッドに切り出し

--- app/models/bought.rb
+++ app/models/bought.rb
@@ -1,13 +1,14 @@
 class Bought < ApplicationRecord
+  BANK_MERGED_DATE = Date.new(2017, 10, 1)
+
   #
   # ZaimAPIを用いてテーブルを最新の状態にする
   #
   def self.synchronize
     api_response = Zaim.new.fetch_pets_payments
-    bank_merged_date = Date.new(2017, 10, 1)
     boughts = api_response.map do |r|
       {
-        amount:  Date.parse(r['date']) < bank_merged_date ? r['amount'] * 2 : r['amount'],
+        amount:  actual_amount(r),
         comment: r['comment'],
         place:   r['place'],
         date:    r['date']
@@ -18,4 +19,18 @@ class Bought < ApplicationRecord
       self.create(boughts)
     end
   end
+
+  private_class_method
+
+    #
+    # 支出情報から金額を取得する
+    # 金額は口座合併の都合上、2017/10/1以前の支払いについては2倍する
+    #
+    def self.actual_amount(payment)
+      if Date.parse(payment['date']) < BANK_MERGED_DATE
+        payment['amount'] * 2
+      else
+        payment['amount']
+      end
+    end
 end
