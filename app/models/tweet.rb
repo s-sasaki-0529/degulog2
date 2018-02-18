6ccf268a190d8c1cfb075bbc7a33e60394d56836
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-02-18 12:10:43 +0000

    fix Tweet#scrape_pictures_urlをprivateに変更

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -9,22 +9,24 @@ class Tweet < ApplicationRecord
     self.transaction do
       tl.each_with_index do |t, idx|
         self.find_or_initialize_by(origin_id: t[:origin_id]).update(t)
-        Tweet.find_by(origin_id: t[:origin_id]).scrape_pictures_url
+        Tweet.find_by(origin_id: t[:origin_id]).__send__(:scrape_pictures_url)
       end
     end
   end
 
-  #
-  # HTTPを用いてツイートに添付される画像URLを同期
-  #
-  def scrape_pictures_url
-    return if self.pictures.present?
-    pictures = Twitter.new.get_pictures_url(self.origin_id)
-    pictures.each do |picture_url|
-      unless self.pictures.where(url: picture_url).exists?
-        self.pictures.create(url: picture_url)
+  private
+
+    #
+    # HTTPを用いてツイートに添付される画像URLを同期
+    #
+    def scrape_pictures_url
+      return if self.pictures.present?
+      pictures = Twitter.new.get_pictures_url(self.origin_id)
+      pictures.each do |picture_url|
+        unless self.pictures.where(url: picture_url).exists?
+          self.pictures.create(url: picture_url)
+        end
       end
     end
-  end
 
 end
